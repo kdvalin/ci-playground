1b7878fd7be2aa946d42ed2ead8489878610ed02
Keith Valin <kvalin@redhat.com>
2025-01-14 15:26:30 -0500

    Drop repo flag, maybe that's the problem

--- .github/workflows/get_parent_issue.sh
+++ .github/workflows/get_parent_issue.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mentioned_issues=$(gh -R "$1" pr view "$2" --json body | jq -r .body | grep -Eo '#[0-9]+' | sed -e 's/#//g')
+mentioned_issues=$(gh pr view "$1" --json body | jq -r .body | grep -Eo '#[0-9]+' | sed -e 's/#//g')
 
 for issue in $mentioned_issues; do
 	if gh issue view "$issue" --json id > /dev/null; then
